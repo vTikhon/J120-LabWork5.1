5d07b284584ba7c7979c016d4e318d56d2d9fb54
vTikhon <tikhon.v@gmail.com>
2022-05-22 14:28:29 +0300

    complete

--- src/ru/avalon/vergentev/j120/labwork5a/TxtViewer.java
+++ src/ru/avalon/vergentev/j120/labwork5a/TxtViewer.java
@@ -6,28 +6,22 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 public class TxtViewer extends JFrame implements KeyListener {
     private static final JScrollPane panelForTree = new JScrollPane();
     private static final JScrollPane panelForText = new JScrollPane();
     private static final JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panelForTree, panelForText);
 
-    private DefaultTreeModel defaultTreeModel;
     private File file;
     private static final DefaultMutableTreeNode nodeStart = new DefaultMutableTreeNode("<<..>>");
     private TreePath path;
     private JTree tree;
-
     private StringBuilder text;
     private static final JTextArea textArea = new JTextArea("");
 
-    Map<String, File> filesMap = new HashMap<>();
-
     public TxtViewer() {
         initializationFrame();
-        initializationTree();
+        buildTree(System.getProperty("user.dir"));
         initializationTextArea();
     }
 
@@ -44,7 +38,7 @@ public class TxtViewer extends JFrame implements KeyListener {
     public MutableTreeNode getDirectoriesForTree (DefaultMutableTreeNode node, File file) {
         DefaultMutableTreeNode nodeChild = new DefaultMutableTreeNode(file.getName());
         node.add(nodeChild);
-        filesMap.put(file.getName(), file);
+//        filesMap.put(file.getName(), file);
         if (file.isDirectory() && file.exists()) {
             File[] listFiles = file.listFiles();
             if (listFiles != null) {
@@ -54,10 +48,11 @@ public class TxtViewer extends JFrame implements KeyListener {
         return node;
     }
 
-    public void initializationTree () {
-        file = new File(System.getProperty("user.dir"));
-        defaultTreeModel = new DefaultTreeModel(getDirectoriesForTree(nodeStart, file));
-        tree = new JTree(defaultTreeModel);  //new DefaultTreeModel(nodeStart)
+    public void buildTree (String directory) {
+        nodeStart.removeAllChildren();
+        file = new File(directory);
+        DefaultTreeModel defaultTreeModel = new DefaultTreeModel(getDirectoriesForTree(nodeStart, file));
+        tree = new JTree(defaultTreeModel);
         addTree();
     }
 
@@ -65,7 +60,7 @@ public class TxtViewer extends JFrame implements KeyListener {
         panelForTree.setViewportView(tree);
         tree.setRootVisible(true);
         tree.setBackground(Color.WHITE);
-        tree.addTreeSelectionListener(e -> valueChanged(e));
+        tree.addTreeSelectionListener(this::valueChanged);
         tree.addKeyListener(this);
     }
 
@@ -78,20 +73,11 @@ public class TxtViewer extends JFrame implements KeyListener {
         DefaultMutableTreeNode node = (DefaultMutableTreeNode) e.getPath().getLastPathComponent();
         //часть для перестроения <<..>> в родительскую директорию
         if (e.getPath().getLastPathComponent() == nodeStart && file.getParentFile() != null) {
-            nodeStart.removeAllChildren();
-            file = new File(file.getParent());
-            defaultTreeModel = new DefaultTreeModel(getDirectoriesForTree(nodeStart, file));
-            tree = new JTree(defaultTreeModel);
-            addTree();
+            buildTree(file.getParent());
         }
         //часть для перестроения <<..>> в наследниковскую директорию
-        else if (!((DefaultMutableTreeNode) e.getPath().getLastPathComponent()).isLeaf() && e.getPath().getLastPathComponent() != nodeStart.getLastChild()) {  // && file.isDirectory ?
-            String key = tree.getSelectionPath().getLastPathComponent().toString();
-            nodeStart.removeAllChildren();
-            file = new File(String.valueOf(filesMap.get(key)));  //String.valueOf(e.getPath().getLastPathComponent())
-            defaultTreeModel = new DefaultTreeModel(getDirectoriesForTree(nodeStart, file));
-            tree = new JTree(defaultTreeModel);
-            addTree();
+        else if (!((DefaultMutableTreeNode) e.getPath().getLastPathComponent()).isLeaf() && e.getPath().getLastPathComponent() != nodeStart.getLastChild()) {
+            buildTree(String.valueOf(getDirectoryFromPath()));
         }
         //чтение txt файлов
         setTitle(String.valueOf(node));
@@ -104,6 +90,17 @@ public class TxtViewer extends JFrame implements KeyListener {
         if (!((DefaultMutableTreeNode) e.getPath().getLastPathComponent()).isLeaf()) {
             path = e.getPath();
         }
+    }
+
+    public StringBuilder getDirectoryFromPath () {
+        StringBuilder directory = new StringBuilder(file.getParent() + "\\");
+        String pathCut = String.valueOf(tree.getSelectionPath()).replaceAll("\\[", "").replaceAll("]", "");
+        String [] pathCutArray = pathCut.split(", ");
+        for (int i = 1; i < pathCutArray.length; i++) {
+            pathCutArray[i] = pathCutArray[i] + "\\";
+            directory.append(pathCutArray[i]);
+        }
+        return directory;
     }
 
     public StringBuilder reader (File file) {
